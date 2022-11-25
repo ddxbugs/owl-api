b15bea1505c2fab4c478ebbba690fc63b1716a3e
ddxbugs <ddxbugs@proton.me>
2022-11-24 21:15:53 -0800

    index.js read json heroes file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-document.onload(function() {
-    $("")
+// index.js
+console.log("index.js")
+
+$.getJSON("index.json", function(data) {
+    $.each(data.profile_pic, function(o) {
+        console.log(o)
+    })
 })
