af178cacd1d7924224f10fe5d69a84d0e12c4527
ddxbugs <ddxbugs@proton.me>
2022-11-28 23:15:14 -0800

    index.js add battle net profile card class constructor api

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 // index.js
 console.log("index.js")
 
-$.getJSON("index.json", function(data) {
-    $.each(data.profile_pic, function(o) {
-        console.log(o)
+var owl_url = "https://api.overwatchleague.com/"
+
+class Hero {
+    constructor() {
+        this.user = "username"
+        this.secret = "accesstoken"
+    }
+
+    async getUser(id) {
+        const url = "https://ow-api.com/v1/stats/pc/us/`$id`/profile"
+        const req = await fetch(url)
+        const res = await req.json()
+        return {res}
+    }
+
+    async displayUser(id) {
+        let html = `<div></div>`
+        $("")
+    }
+}
+
+let hero = null
+
+document.onload = function() {
+    // display heroes
+    $.getJSON("index.json", function(data) {
+        $.each(data.profile_pic, function(o) {
+            console.log(o)
+        })
     })
-})
+
+    // mouse click listener
+    
+    // create mouse hover event handler
+    $("").on("keyup", function(e) {
+        console.log(e)
+        if (e) {
+            hero.getProfile(e.target.value).then(data => {
+                if (data) {
+                    hero.showData()
+                } else {
+                    console.log(data)
+                }
+            })
+        }
+    })
+
+}
+
